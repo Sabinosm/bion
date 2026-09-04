47d82ea325126e5f19f35c54f13590a952142ac4
Pedro Sabino <22402721@aluno.cotemig.com.br>
2026-09-03 22:39:36 -0300

    fix: add missing origin to CORS allowed origins list

--- src/main.py
+++ src/main.py
@@ -34,6 +34,7 @@ def create_app(config_name: str = "development") -> Flask:
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://sabinosm.github.io",
+        "https://bion-one.vercel.app"
     ]
 
     CORS(
