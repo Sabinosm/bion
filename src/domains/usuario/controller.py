f4414e34724dbe605da136e6d73312bf093878f6
Pedro Sabino <22402721@aluno.cotemig.com.br>
2026-09-10 09:07:30 -0300

    feat: update StepUp confirmation requirement for activating professionals

--- src/domains/usuario/controller.py
+++ src/domains/usuario/controller.py
@@ -150,7 +150,7 @@ class UsuarioController():
     @staticmethod
     @bp.post("/<uuid>/ativar")
     @requer_admin
-    @StepUp.requer_confirmacao_recente("desativar_profissional")
+    @StepUp.requer_confirmacao_recente("ativar_profissional")
     def ativar(uuid):
         try:
             u = _svc.ativar(uuid, solicitante_eh_super_admin=g.is_super_admin)
